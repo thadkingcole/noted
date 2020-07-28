12b7a6cdc107533b364caec221fe354edc8d9ffe
Thad Cole <thadjcole@gmail.com>
2020-07-28 10:00:59 -0400

    set up server to listen on port 5404

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 // required modules
 const express = require("express");
+const path = require("path");
+
+// set up express app
+const app = express();
+const PORT = process.env.PORT || 5404;
+
+// set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// starts server to begin listening
+app.listen(PORT, () => console.log("listening on PORT: ", PORT));
